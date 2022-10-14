5c12a529621953896a132e7528bbab491a657775
kajal-max <kajal.mm@outlook.com>
2022-10-14 11:26:15 -0700

    status update

---
+++ EXERCISE - 1.sql
@@ -0,0 +1,8 @@
+create  table HOTEL_MANAGEMENT_SYSTEM 
+(Cust_Id int primary key,
+ Cust_Name varchar(30),
+ Cust_City varchar(50), 
+ Cust_Room_No int, 
+ CheckIn_Time datetime);
+  select * from HOTEL_MANAGEMENT_SYSTEM
+ 
